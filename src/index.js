ee2198a728a2d685cef2d26a46e7d90dddcbe6b4
Paul Iambaev <slushau.vnimatelno@yandex.ru>
2021-02-10 22:28:44 +0500

    comlpete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    let expression = (function (conf) {
+        let rE = "";
+        for (const bracket of conf) {
+            rE +=
+                conf.length > 1
+                    ? "(\\" + bracket.join("\\") + ")|"
+                    : "\\" + bracket.join("\\");
+        }
+
+        rE = conf.length > 1 ? rE.slice(0, -1) : rE;
+
+        if (/\d/.test(rE)) {
+            rE = rE.replace(/\\/g, "");
+        }
+
+        return rE;
+    })(bracketsConfig);
+
+
+    const regExp = new RegExp(expression, "g");
+
+    while (str.length && regExp.test(str)) {
+        str = str.replace(regExp, "");
+    }
+
+    return str.length === 0;
 }
